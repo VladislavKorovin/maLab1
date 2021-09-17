da80c4549aebd90ecb91e7a88c271e57be37998b
vKorovin <vladislav.korovin@leroymerlin.ru>
2021-09-17 16:35:05 +0300

    Добавил выделение экспоненты и мантиссы для нормализованных чисел, округление, вывод числа в жкспоненциальном виде

--- src/Application.kt
+++ src/Application.kt
@@ -1,7 +1,14 @@
+import java.math.BigDecimal
+
 fun main(){
-    val a = HugeDecimal("2.000000000000")
-    val b = HugeDecimal("-2.0000000100000")
-    val c = HugeDecimal("-2.00000000")
-    val d = HugeDecimal("2.00002000000")
-    print("")
+    val a = HugeDecimal("0")
+    val b = HugeDecimal("0.00001234000")
+    val c = HugeDecimal("2123124")
+    val d = HugeDecimal("132.13948100")
+    val e = HugeDecimal("-0.00002")
+    print(a.exponentialView() + "\n")
+    print(b.exponentialView()+ "\n")
+    print(c.exponentialView()+ "\n")
+    print(d.exponentialView()+ "\n")
+    print(e.exponentialView()+ "\n")
 }
--- src/HugeDecimal.kt
+++ src/HugeDecimal.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 class HugeDecimal(input: String) {
 
     private var listRepresentation: MutableList<Char> //Представление числа в виде изменяемого (mutable) списка
@@ -13,12 +15,9 @@ class HugeDecimal(input: String) {
             listRepresentation.remove('-') //Удаляем минус (мы уже знаем знак числа, в списке минус будет мешать)
             dropZeros() //Отбрасываем незначащие нули дробной части
             dotIndex = listRepresentation.indexOf('.') //Узнаем, какой индекс имеет разделительная точка
-
-
-            /*if (representationMode == RepresentationMode.NORMALIZED)
-                getNormalizedMantissaAndExponent()*/
-
-
+            if (representationMode == RepresentationMode.NORMALIZED)
+                getNormalizedMantissaAndExponent()
+            round()
         } else
             throw IllegalInputFormatException("Input string has incorrect format: $input")
     }
@@ -43,24 +42,67 @@ class HugeDecimal(input: String) {
         } else return
     }
 
-    //TODO реализовать функции получения мантиссы и экспоненты
     private fun getNormalizedMantissaAndExponent() { //Метод выделения мантиссы и порядка (экспоненты) в нормализованном виде
-
+        if (listRepresentation[0] == '0' && listRepresentation.indexOf('.') == 1) { //если первая цифра числа - ноль
+            val firstNotNullIndex =
+                listRepresentation.indexOfFirst { it != '0' && it != '.' } //получаем индекс первой ненулевой цифры
+            exponent = dotIndex - firstNotNullIndex //получаем экспоненту
+            mantissa = listRepresentation.toMutableList()
+            Collections.rotate(listRepresentation, -firstNotNullIndex)
+            listRepresentation.remove('.')
+            listRepresentation.add(1, '.')
+            dropZeros()
+            val buffer = mantissa.toMutableList()
+            mantissa = listRepresentation.toMutableList()
+            listRepresentation = buffer.toMutableList()
+        } else if (dotIndex != -1 && listRepresentation.count() > 1) {
+            exponent = dotIndex - 1
+            mantissa = listRepresentation.toMutableList()
+            mantissa.remove('.')
+            mantissa.add(1, '.')
+        } else if (dotIndex == -1 && listRepresentation.count() > 1) {
+            exponent = listRepresentation.count() - 1
+            mantissa = listRepresentation.toMutableList()
+            mantissa.add(1, '.')
+        } else if (listRepresentation.count() == 1) {
+            mantissa = listRepresentation.toMutableList()
+            exponent = 0
+        }
     }
 
+    //TODO реализовать ненормализованные числа
     private fun getDenormalizedMantissaAndExponent() { //Метод выделения мантиссы и порядка (экспоненты) в ненормализованном виде
 
     }
 
-    //TODO реализовать функцию округления
+    //TODO последние два способа округления реализованы плохо, перереализовать
     private fun round() { //Метод округления числа (если мантиссы не хватает)
+        if (mantissa.count() > mantissaLength + 1) {
+            if (Config.roundingMode == RoundingMode.DROP) { //Режим отбрасывания
+                mantissa = mantissa.take(mantissaLength + 1).toMutableList()
+            }
+            if (Config.roundingMode == RoundingMode.HALF_UP) { //Округление вверх
+                if (mantissa[mantissaLength + 2].toInt() >= 5) {
+                    mantissa = mantissa.take(mantissaLength + 1).toMutableList()
+                    mantissa[mantissaLength + 1] = mantissa[mantissaLength + 1].toInt().inc().toChar()
+                } else
+                    mantissa = mantissa.take(mantissaLength + 1).toMutableList()
+            }
+            if (Config.roundingMode == RoundingMode.HALF_DOWN) { //Округление вниз
+                if (mantissa[mantissaLength + 2].toInt() >= 5) {
+                    mantissa = mantissa.take(mantissaLength + 1).toMutableList()
+                    mantissa[mantissaLength + 1] = mantissa[mantissaLength + 1].toInt().dec().toChar()
+                } else
+                    mantissa = mantissa.take(mantissaLength + 1).toMutableList()
+            }
 
+        }
     }
 
     //Перегружаем операторы +,-,*
     //TODO реализовать перегрузку операторов
     operator fun plus(other: HugeDecimal): HugeDecimal {
-        return HugeDecimal("100.0000")
+        return HugeDecimal("100.0")
     }
 
     operator fun minus(other: HugeDecimal): HugeDecimal {
@@ -71,17 +113,25 @@ class HugeDecimal(input: String) {
         return HugeDecimal("300.0")
     }
 
+    fun exponentialView(): String {
+        var result = mantissa.joinToString("") + "*$base^$exponent"
+        if (isNegative)
+            result = "-$result"
+        return result
+    }
+
     companion object Config {
         private var mantissaLength: Int = 5 //Длина мантиссы
         private val base: Int = 10 //Основание системы счисления
-        private var roundingMode: RoundingMode = RoundingMode.HALF_UP //Режим округления
-        private var representationMode = RepresentationMode.NORMALIZED //Способ представления числа (нормализованный или нет)
+        private var roundingMode: RoundingMode = RoundingMode.DROP //Режим округления
+        private var representationMode =
+            RepresentationMode.NORMALIZED //Способ представления числа (нормализованный или нет)
 
-        fun setRoundingMode(roundingMode: RoundingMode){ //Метод установки режима округления
+        fun setRoundingMode(roundingMode: RoundingMode) { //Метод установки режима округления
             Config.roundingMode = roundingMode
         }
 
-        fun setRepresentationMode(representationMode: RepresentationMode){ //Метод установки способа представления числа
+        fun setRepresentationMode(representationMode: RepresentationMode) { //Метод установки способа представления числа
             Config.representationMode = representationMode
         }
     }
